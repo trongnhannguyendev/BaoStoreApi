9759894acfda7e5aedb54ae5f1926d68b4de0e5a
trongnhannguyendev <124512127+trongnhannguyendev@users.noreply.github.com>
2023-03-18 15:10:58 +0700

    add image service + views + controller

--- controllers/image-controller.php
+++ controllers/image-controller.php
@@ -8,4 +8,24 @@ class ImagesController
     {
         $this->services = new ImagesServices();
     }
+    public function insertImages($data)
+    {
+        return $this->services->insertImages($data);
+    }
+    public function removeImages($imageid)
+    {
+        return $this->services->removeImages($imageid);
+    }
+    public function updateImages($data)
+    {
+        return $this->services->updateImages($data);
+    }
+    public function changeDefaultAllow($imageid)
+    {
+        return $this->services->changeDefaultAllow($imageid);
+    }
+    public function changeDefaultDeny($imageid)
+    {
+        return $this->services->changeDefaultDeny($imageid);
+    }
 }
--- services/images-services.php
+++ services/images-services.php
@@ -12,11 +12,150 @@ class ImagesServices
     }
     public function insertImages($data)
     {
+        $response = Response::getDefaultInstance();
+        try {
+            $query = "INSERT INTO TBLIMAGES SET URL = ?, ISDEFAULT = 0, BOOKID = ?";
+            $stmt = $this->connect->prepare($query);
+            $stmt->bindParam(1, $data->url);
+            $stmt->bindParam(2, $data->bookid);
+            $this->connect->beginTransaction();
+
+            if ($stmt->execute()) {
+                $this->connect->commit();
+                $response->setMessage("Insert image success");
+                $response->setError(false);
+                $response->setResponeCode(1);
+            } else {
+                $this->connect->rollBack();
+                $response->setMessage("Insert image failed");
+                $response->setError(true);
+                $response->setResponeCode(0);
+            }
+        } catch (Exception $e) {
+
+            $response->setMessage("Have issue with DB" . $e->getMessage());
+            $response->setError(true);
+            $response->setResponeCode(5);
+        }
+        return $response;
     }
-    public function removeImages($data)
+    public function removeImages($imageid)
     {
+
+        $response = Response::getDefaultInstance();
+        try {
+            $query = "DELETE FROM TBLIMAGES WHERE IMAGEID = ?  AND ISDEFAULT = 0";
+            $stmt = $this->connect->prepare($query);
+            $stmt->bindParam(1, $imageid);
+            $this->connect->beginTransaction();
+
+            if ($stmt->execute()) {
+                $this->connect->commit();
+                $response->setMessage("Remove image success");
+                $response->setError(false);
+                $response->setResponeCode(1);
+            } else {
+                $this->connect->rollBack();
+                $response->setMessage("Remove image failed");
+                $response->setError(true);
+                $response->setResponeCode(0);
+            }
+        } catch (Exception $e) {
+
+            $response->setMessage("Have issue with DB" . $e->getMessage());
+            $response->setError(true);
+            $response->setResponeCode(5);
+        }
+        return $response;
     }
+
     public function updateImages($data)
     {
+
+        $response = Response::getDefaultInstance();
+        try {
+            $query = "UPDATE FROM TBLIMAGES SET URL =? WHERE IMAGEID = ?";
+            $stmt = $this->connect->prepare($query);
+            $stmt->bindParam(1, $data->url);
+            $stmt->bindParam(2, $data->imageid);
+            $this->connect->beginTransaction();
+
+            if ($stmt->execute()) {
+                $this->connect->commit();
+                $response->setMessage("Update image success");
+                $response->setError(false);
+                $response->setResponeCode(1);
+            } else {
+                $this->connect->rollBack();
+                $response->setMessage("Update image failed");
+                $response->setError(true);
+                $response->setResponeCode(0);
+            }
+        } catch (Exception $e) {
+
+            $response->setMessage("Have issue with DB" . $e->getMessage());
+            $response->setError(true);
+            $response->setResponeCode(5);
+        }
+        return $response;
+    }
+
+    public function changeDefaultAllow($imageid)
+    {
+
+        $response = Response::getDefaultInstance();
+        try {
+            $query = "UPDATE FROM TBLIMAGES SET ISDEFAULT =1 WHERE IMAGEID = ?";
+            $stmt = $this->connect->prepare($query);
+            $stmt->bindParam(1, $imageid);
+            $this->connect->beginTransaction();
+
+            if ($stmt->execute()) {
+                $this->connect->commit();
+                $response->setMessage("Change default image success");
+                $response->setError(false);
+                $response->setResponeCode(1);
+            } else {
+                $this->connect->rollBack();
+                $response->setMessage("Change default image failed");
+                $response->setError(true);
+                $response->setResponeCode(0);
+            }
+        } catch (Exception $e) {
+
+            $response->setMessage("Have issue with DB" . $e->getMessage());
+            $response->setError(true);
+            $response->setResponeCode(5);
+        }
+        return $response;
+    }
+    public function changeDefaultDeny($imageid)
+    {
+
+        $response = Response::getDefaultInstance();
+        try {
+            $query = "UPDATE FROM TBLIMAGES SET ISDEFAULT =0 WHERE IMAGEID = ?";
+            $stmt = $this->connect->prepare($query);
+            $stmt->bindParam(1, $imageid);
+            $this->connect->beginTransaction();
+
+            if ($stmt->execute()) {
+                $this->connect->commit();
+                $response->setMessage("Change default image success");
+                $response->setError(false);
+                $response->setResponeCode(1);
+            } else {
+                $this->connect->rollBack();
+                $response->setMessage("Change default image failed");
+                $response->setError(true);
+                $response->setResponeCode(0);
+            }
+        } catch (Exception $e) {
+
+            $response->setMessage("Have issue with DB" . $e->getMessage());
+            $response->setError(true);
+            $response->setResponeCode(5);
+        }
+        return $response;
     }
 }
